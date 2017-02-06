b92284d29c10725b96cb46d4402f643fd15e266e
Mohamed-Elfatih Abdelmagid <ma11415@eews304-027.ee.ic.ac.uk>
2017-02-06 15:49:57 +0000

    First commit

---
+++ src/udp/UDPServer.java
@@ -0,0 +1,142 @@
+/*
+ * Created on 01-Mar-2016
+ */
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+
+import common.MessageInfo;
+
+public class UDPServer {
+
+	private DatagramSocket recvSoc;
+	private int totalMessages = -1;
+	private int[] receivedMessages;
+	private boolean close;
+
+	private void run() {
+		int				pacSize;
+		byte[]			pacData;
+		DatagramPacket 	pac;
+
+		// TO-DO: Receive the messages and process them by calling processMessage(...).
+		//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
+		
+		pacSize = 1024;
+		pacData = new byte[pacSize];
+		
+		try{
+			while (true){
+				// useless khara a7a 
+				for (int n = 0; n < pacSize; n++){
+					pacData[n] = 0;
+				}
+				
+				pac = new DatagramPacket(pacData, pacSize);
+				
+				try{
+					recvSoc.setSoTimeout(30000);
+					recvSoc.receive(pac);
+					processMessage(new String(pac.getData()));
+				} catch (SocketTimeoutException e){
+					System.out.println("Socket Timedout");
+					//printSummary();
+				}
+				
+			}
+		} catch(SocketException e){
+			System.out.println("Socket exception: " + e.getMessage());
+		} catch(IOException e){
+			System.out.println("IO exception: " + e.getMessage());
+		}
+	}
+
+	public void processMessage(String data) {
+
+		MessageInfo msg = null;
+
+		// TO-DO: Use the data to construct a new MessageInfo object
+		
+		try {
+			msg = new MessageInfo(data.trim());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		// TO-DO: On receipt of first message, initialise the receive buffer
+		if (receivedMessages == null){
+			totalMessages = 0;
+			receivedMessages = new int[msg.totalMessages];
+		}
+		// TO-DO: Log receipt of the message
+		
+		totalMessages++;
+		receivedMessages[msg.messageNum] = 1;
+		
+		// TO-DO: If this is the last expected message, then identify
+		//        any missing messages
+		
+		if (totalMessages == msg.totalMessages){
+			printSummary();
+		}
+	}
+
+
+	public UDPServer(int rp) {
+		// TO-DO: Initialise UDP socket for receiving data
+		try{
+			recvSoc = new DatagramSocket(rp);
+		} catch (SocketException e){
+			System.out.println("Socket: " + e.getMessage());
+		}
+		// Done Initialisation
+		System.out.println("UDPServer ready");
+	}
+
+	public static void main(String args[]) {
+		int	recvPort;
+
+		// Get the parameters from command line
+		if (args.length < 1) {
+			System.err.println("Arguments required: recv port");
+			System.exit(-1);
+		}
+		recvPort = Integer.parseInt(args[0]);
+
+		// TO-DO: Construct Server object and start it by calling run().
+		UDPServer server = new UDPServer(recvPort);
+		server.run();
+	}
+	
+	public void printSummary(){
+
+		if(receivedMessages == null || totalMessages <= 0){
+			return;
+		}
+		
+		String missingMessages = "";
+		for(int i = 0; i <receivedMessages.length; i++){
+			if(receivedMessages[i] == 0){
+				missingMessages += i + ", ";
+			}
+		}	
+	
+		System.out.println("Number of messages received: " + totalMessages);
+		if(totalMessages == receivedMessages.length){
+			System.out.println("No missing messages");
+		}else{
+			System.out.println("Lost Messages: " + missingMessages);
+		}
+		receivedMessages = null;
+		totalMessages = -1;
+
+
+
+}
+
+}
